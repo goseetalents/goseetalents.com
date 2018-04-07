15324008128c6a0445003fd6d503c611d7642dae
Fredrik Rubensson <fredrik.rubensson@eniro.com>
2018-04-07 20:33:00 +0200

    fixed the menu once and for all ?!? :-)

--- content/js/new.js
+++ content/js/new.js
@@ -1,6 +1,19 @@
 
 ;(function($){
-    $('.menu-item').click(function(){
+    $('.menu-item').click(function(event){
+        event.preventDefault()
+        var targetUrl = $(this).children().attr('href')
+        var scrollToId = targetUrl.split('/').pop()
+        var offset = $(scrollToId).offset().top
+
+        if(targetUrl.match(/stockholm/)) {
+            offset = offset - 380
+        } else {
+            offset = offset - 69
+        }
+
+        $(window).scrollTop(offset)
+
         $('header').removeClass('menu-open')
         $('.menu-container').hide()
     });
